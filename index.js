834c5304b95b9ecb88e545a6f8b80dfbd86c9e24
Marlowe Recht <recht.marlowe@gmail.com>
2022-12-13 22:48:51 +0100

    commit tree 156e51a7ddf4bebae7d260a581da89922ddc3b84

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(array) {
+    const firstTwo = array.slice(0, 2);
+    return firstTwo;
+}
+
+returnFirstTwoDrivers(drivers);
+
+const returnLastTwoDrivers = function(array) {
+    const lastTwo = array.slice (2);
+    return lastTwo;
+}
+
+returnLastTwoDrivers(drivers);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const hello = function(x) {
+    x*x;
+}
+
+function createFareMultiplier(x) {
+    return function(x) {
+        return x*x;
+    }
+}
+
+function fareDoubler(y) {
+    return y*2;
+}
+
+function fareTripler(y) {
+    return y*3;
+}
+
+function selectDifferentDrivers(array, func) {
+
+    return func(array);
+
+}
